1d3b55546b3cc12df08d660d55acb882107be7a0
NYPDK <45737840+NYPDK@users.noreply.github.com>
2020-11-07 00:47:44 -0600

    Update PyTypeRacer.py
    Changed some stuff and added a check for the "anti cheater test" so it will skip it when you go again.

--- PyTypeRacer.py
+++ PyTypeRacer.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
-import time, re
+import time, re, os
 
-default_delay = 0.105
+clear = lambda: os.system('cls')
+default_delay = 0.103
+clear()
 usr_delay = input('Enter a delay value (Press enter to use default: ' + str(default_delay) + '): ')
 
 if usr_delay != '':
-    print('\nDelay set to ' + str(usr_delay))
+    clear()
+    print('Delay set to ' + str(usr_delay))
     delay = float(usr_delay)
 else:
-    print('\nNo delay specified - using default delay of ' + str(default_delay))
+    clear()
+    print('No delay specified - using default delay of ' + str(default_delay))
     delay = default_delay
 
 browser = webdriver.Chrome(service_log_path='NUL')
@@ -20,14 +24,18 @@ def start():
     while timer == True:
         try:
             browser.find_element_by_xpath('//*[@class="txtInput txtInput-unfocused"]')
-            print('Waiting for countdown')
+            count_down = browser.find_element_by_xpath('(//*[@class="time"])[2]').text
+            if count_down != '' and count_down != ':00':
+                clear()
+                print('Starting in: ' + re.sub(':', '', count_down))
         except:
             timer = False
+            clear()
             print('Count down complete! Starting!\n')
             main()
-        time.sleep(0.5)
+        time.sleep(1)
 
-# Handles all the logic
+# Handles the reading and writting of the phrase
 def main():
     try:
         typeBar = browser.find_element_by_class_name('txtInput')
@@ -37,7 +45,8 @@ def main():
 
         word_check = (word1 + wordcomma + ' ' + word2)
         word_complete = re.sub(' , ', ', ', word_check)
-        print('Words: \n' + word_complete)
+        clear()
+        print('Words: \n' + word_complete + '\n')
         for char in word_complete:
             typeBar.send_keys(char)
             time.sleep(delay)
@@ -49,45 +58,70 @@ def main():
 
         word_check = (word1 + ' ' + word2)
         word_complete = re.sub(' , ', ', ', word_check)
-        print('Words: \n' + word_complete)
+        clear()
+        print('Words: \n' + word_complete + '\n')
         for char in word_complete:
             typeBar.send_keys(char)
             time.sleep(delay)
         loop()
 
-# Used to re-run the script
+# Re-run the script
 def loop():
-    time.sleep(2)
-    input('\n\n\nPress enter to race again! \n(Make sure to close any pop-up windows first!)\n\n\n')
+    clear()
+    input('Press enter to race again! \n(Make sure to close any pop-up windows first!)\n\n\n')
     print('Bot started, joining race!')
     try:
-        browser.find_element_by_link_text('Enter a typing race').click()
-        time.sleep(3)
-        start()
-    except:
-        browser.find_element_by_link_text('Race Again »').click()
+        browser.find_element_by_xpath('//*[@type="button"]').click()
+        time.sleep(0.5)
+        browser.find_element_by_xpath('//*[@type="button"]').click()
+        time.sleep(0.5)
+        browser.find_element_by_xpath('//*[@class="xButton"]').click()
         time.sleep(1)
         try:
-            browser.find_element_by_link_text('No thanks :(').click()
+            browser.find_element_by_link_text('Enter a typing race').click()
             time.sleep(3)
             start()
         except:
+            browser.find_element_by_link_text('Race Again »').click()
+            time.sleep(1)
+            try:
+                browser.find_element_by_link_text('No thanks :(').click()
+                time.sleep(3)
+                start()
+            except:
+                time.sleep(3)
+                start()
+    except:
+        try:
+            browser.find_element_by_link_text('Enter a typing race').click()
             time.sleep(3)
             start()
+        except:
+            browser.find_element_by_link_text('Race Again »').click()
+            time.sleep(1)
+            try:
+                browser.find_element_by_link_text('No thanks :(').click()
+                time.sleep(3)
+                start()
+            except:
+                time.sleep(3)
+                start()
 
 # Initial start
 time.sleep(1.5)
-input('\n\n\nPress enter to begin \n(Wait until you are done loading and are on the menu screen)\n\n\n')
+clear()
+input('Press enter to begin \n(Wait until you are done loading and are on the menu screen)\n\n\n')
 print('Bot started, joining race!')
 try:
     browser.find_element_by_link_text('Enter a typing race').click()
 except:
+    clear()
     print('You need to wait for the site to finish loading!!!\nExiting program!')
     time.sleep(3)
-time.sleep(1)
+time.sleep(0.5)
 browser.find_element_by_link_text('change display format').click()
-time.sleep(1)
+time.sleep(0.5)
 browser.find_element_by_xpath('(//*[@type="radio"])[2]').click()
-time.sleep(1)
+time.sleep(0.5)
 browser.find_element_by_xpath('//*[@title="close this popup"]').click()
 start()
